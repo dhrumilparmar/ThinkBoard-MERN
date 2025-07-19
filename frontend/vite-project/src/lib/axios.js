133e9fc9ece33452650102fa1746a5b271ac0133
dhupe <parmardhrumil667@gmail.com>
2025-07-20 01:54:58 +0530

    prepare axios for deployment

--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:5000/api" : "/api";
+// In production, use relative path since frontend and backend are served from same domain
+const BASE_URL = import.meta.env.MODE === "production" ? "/api" : "http://localhost:5000/api";
+
 const api = axios.create({
   baseURL: BASE_URL,
 });
