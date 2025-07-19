8e9725b9c0f1b7e5eb5209d2cee8ef201f69b1fa
dhupe <parmardhrumil667@gmail.com>
2025-07-20 01:39:32 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';//module type
 import cors from 'cors'; //for cross-origin resource sharing
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import notesroutes from './routes/notesroutes.js';
 import { connectdb } from './config/db.js';
@@ -9,11 +11,18 @@ import ratelimiter from './middleware/rateLimiter.js';
 dotenv.config(); // Load environment variables from .env file
 const PORT = process.env.PORT || 5000; 
 
+// Get __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //middleware 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173', // Adjust this to your frontend URL
-}));
+if(process.env.NODE_ENV !== "production"){
+
+    app.use(cors({
+        origin: 'http://localhost:5173', // Adjust this to your frontend URL
+    }));
+}
 app.use(express.json())
 app.use(ratelimiter);
 app.use('/api/notes', notesroutes)
@@ -23,7 +32,12 @@ app.use('/api/notes', notesroutes)
 //     next();
 // }
 
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../../frontend/vite-project/dist")))
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../../frontend/vite-project/dist/index.html"))
+    });
+}
 connectdb().then(()=>{
     app.listen(PORT, () =>{
         console.log(`Server is running on port http://localhost:${PORT}`);
